df4bfee0d085aec26cf497ad966318fa63faed63
levlam <levlam@telegram.org>
2026-04-21 15:12:17 +0300

    Add the option "text_composition_style_prompt_length_max".

--- td/telegram/OptionManager.cpp
+++ td/telegram/OptionManager.cpp
@@ -213,6 +213,7 @@ OptionManager::OptionManager(Td *td)
   set_default_integer_option("owned_bot_count_max", 20);
   set_default_integer_option("text_composition_style_example_count", 7);
   set_default_integer_option("text_composition_style_title_length_max", 12);
+  set_default_integer_option("text_composition_style_prompt_length_max", 1024);
 
   if (options.isset("my_phone_number") || !options.isset("my_id")) {
     update_premium_options();
