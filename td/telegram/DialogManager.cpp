a96365b5f0ecd01d6de31ad813f45fa81df5ffb4
levlam <levlam@telegram.org>
2026-05-02 02:51:07 +0300

    Remove special handling in the error "USERNAME_PURCHASE_AVAILABLE".

--- td/telegram/DialogManager.cpp
+++ td/telegram/DialogManager.cpp
@@ -3018,9 +3018,6 @@ void DialogManager::check_dialog_username(DialogId dialog_id, const string &user
         return promise.set_value(CheckDialogUsernameResult::Occupied);
       }
       if (error.message() == "USERNAME_PURCHASE_AVAILABLE") {
-        if (begins_with(G()->get_option_string("my_phone_number"), "1")) {
-          return promise.set_value(CheckDialogUsernameResult::Invalid);
-        }
         return promise.set_value(CheckDialogUsernameResult::Purchasable);
       }
       return promise.set_error(std::move(error));
